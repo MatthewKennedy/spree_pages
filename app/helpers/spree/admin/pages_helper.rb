e3fec3d01a185d8781a23fc3edb08554cc326889
Matthew Kennedy <m.kennedy@me.com>
2020-11-08 20:28:56 +0000

    update link

--- app/helpers/spree/admin/pages_helper.rb
+++ app/helpers/spree/admin/pages_helper.rb
@@ -4,7 +4,12 @@ module Spree
   module Admin
     module PagesHelper
       def view_in_store_link(page, store)
-        link_to(store.unique_name, store.url + page_path(page), class: "dropdown-item py-2 px-3")
+
+        unless store.url[/\Ahttp:\/\//] || store.url[/\Ahttps:\/\//]
+          store_url = "http://#{store.url}"
+        end
+
+        link_to(store.unique_name, store_url + page_path(page), class: "dropdown-item py-2 px-3")
       end
     end
   end
